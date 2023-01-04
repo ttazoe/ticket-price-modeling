5810990a1e30973efcadb01772cdd0e19153dcb1
tatazoe <takashi.tazoe@gmail.com>
2023-01-04 17:49:53 +0900

    :add: Install MyBatis

---
+++ src/test/java/org/example/sample2/infrastructure/PriceCategoryRepositoryTest.java
@@ -0,0 +1,12 @@
+package org.example.sample2.infrastructure;
+
+import java.sql.*;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PriceCategoryRepositoryTest {
+
+
+}
